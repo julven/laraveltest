ee5be00d467f5b08029e3b88875ca5ba4e5a366e
unknown <julven_condor@yahoo.com>
2020-12-08 00:00:38 -0500

    prevent back navigation after logging out

--- app/Http/Middleware/authCheck.php
+++ app/Http/Middleware/authCheck.php
@@ -19,6 +19,8 @@ class authCheck
         if(!Auth::check()) {
             return redirect('login');
         }
-        else return $next($request);
+        else return $next($request)->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+        ->header('Pragma','no-cache')
+        ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
     }
 }
